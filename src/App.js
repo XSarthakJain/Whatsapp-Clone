9267bf07b0dce86bb29b5069c744680f1abd6cc8
Sarthak Jain <sarthakjain31797@gmail.com>
2021-06-26 17:23:58 +0530

    modified in predefinded files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react';
+import './css/App.css';
+let App = ()=>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className="container">
+        <div className="leftDiv">
+          <h1>LeftDiv</h1>
+        </div>
+
+        <div className="rightDiv">
+          <h1>RightDiv</h1>
+        </div>
+      </div>
+    </React.Fragment>
   );
 }
 
