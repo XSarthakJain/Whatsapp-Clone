619af7a8b8c3287f004bec087b9b70ed2994a0d4
Sarthak Jain <sarthakjain31797@gmail.com>
2021-06-27 01:59:27 +0530

    Implement Router in Chat Section

--- src/Rightdiv.jsx
+++ src/Rightdiv.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Emptypage from './Emptypage';
-
+import Userfeed from './Userfeed';
+import {Route,Switch} from 'react-router-dom';
 
 let Rightdiv = ()=>{
     return (
         <React.Fragment>
-            <Emptypage/>
+            <Switch>
+                <Route exact path="/chat" component={Userfeed}/>
+                <Route component={Emptypage}/>
+            </Switch>
         </React.Fragment>
     );
 }
---
+++ src/Userfeed.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+const Userfeed = ()=>{
+    return (
+        <React.Fragment>
+            <h1>UserFeed</h1>
+        </React.Fragment>
+    );
+}
+
+
+
+export default Userfeed;
