c3aa4c19dcdc7935024f0a79c2f95043ac918247
Sarthak Jain <sarthakjain31797@gmail.com>
2021-06-29 12:50:09 +0530

    Implement Status Close Functionality

--- src/Rightstatuswindow.jsx
+++ src/Rightstatuswindow.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import './css/Rightstatuswindow.css';
-
-
+import { NavLink } from 'react-router-dom';
 let Rightstatuswindow = ()=>{
     return (
         <React.Fragment>
             <div className="RightStatusWindow">
             <div className="close">
+            <NavLink exact to="/">
             <span className="material-icons">
                 close
             </span>
+            </NavLink>
             </div>
 
             <div className="statusFeedMessageWrapper">
