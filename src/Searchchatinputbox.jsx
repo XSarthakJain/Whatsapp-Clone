402bc8d16722aa1ff7ed169258305615f7f1c462
Sarthak Jain <sarthakjain31797@gmail.com>
2021-06-30 03:29:46 +0530

    Search Chat Animation Implement

--- src/Searchchatinputbox.jsx
+++ src/Searchchatinputbox.jsx
@@ -4,12 +4,21 @@ import './css/Searchchatinputbox.css';
 import Leftdivsearchbar from './Leftdivsearchbar.jsx';
 
 let Searchchatinputbox = ()=>{
+    setTimeout(()=>{
+            let search = document.getElementsByClassName("LeftdivSearchBarWrapper")[0];
+            if(search!=undefined){
+            search.classList.add("LeftdivSearchBarWrapperAfter");
+        }
+        },100);
     return (
         <React.Fragment>
             <div className="Searchchatinputbox">
             <Profileinfocontaintheader name="SearchMessages"/>
+
+            <div className="LeftdivSearchBarWrapper">
             <Leftdivsearchbar title="Search..."/>
             <p>Search for messages with Self.</p>
+            </div>
             </div>
         </React.Fragment>
     );
